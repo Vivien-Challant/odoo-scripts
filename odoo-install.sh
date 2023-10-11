fc718a3fc533a0ae8ab9bf45769c18c304b62df4
Vivien-Challant <vivien@challant.fr>
2023-10-11 13:37:02 +0200

    Create odoo-install.sh

---
+++ odoo-install.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip -y
+sudo pip3 install xlwt
+sudo pip3 install num2words
+sudo apt install postgresql -y
+wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/16.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
+sudo apt update && sudo apt install odoo 
