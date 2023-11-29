424d93c8d2f0a15de389ea789ef7ef926eebca47
Vivien-Challant <vivien@challant.fr>
2023-11-29 12:39:52 +0100

    created enterprise script

---
+++ odoo-enterprise-install.sh
@@ -0,0 +1,11 @@
+#set the services to auto restart#
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf 
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip -y
+sudo pip3 install xlwt
+sudo pip3 install num2words
+sudo apt install postgresql -y
+#wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
+#echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/17.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
+#sudo apt update && sudo apt install odoo -y
+sudo sed -i 's/#$nrconf{restart} = '"'"'a'"'"';/$nrconf{restart} = '"'"'i'"'"';/g' /etc/needrestart/needrestart.conf 
