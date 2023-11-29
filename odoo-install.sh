a2744ceab50661a52776acdc388f9a295fc8723e
Vivien-Challant <vivien@challant.fr>
2023-11-29 09:40:57 +0100

    Update odoo-install.sh
    Updated script to Odoo 17.0

--- odoo-install.sh
+++ odoo-install.sh
@@ -6,6 +6,6 @@ sudo pip3 install xlwt
 sudo pip3 install num2words
 sudo apt install postgresql -y
 wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
-echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/16.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
+echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/17.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
 sudo apt update && sudo apt install odoo -y
 sudo sed -i 's/#$nrconf{restart} = '"'"'a'"'"';/$nrconf{restart} = '"'"'i'"'"';/g' /etc/needrestart/needrestart.conf 
